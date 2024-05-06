e7f1f88bbaa07b038d4cf018cd15858c8efa79a2
MaskAnonimus <125550747+MaskAnonimus@users.noreply.github.com>
2024-05-06 13:11:04 +0300

    Add files via upload

---
+++ variant1.js
@@ -0,0 +1,92 @@
+let ans1;
+let ans2;
+let ans3;
+let ans4;
+let ans5;
+let ans6;
+let ans7;
+let ans8;
+let ans9;
+let ans10;
+let shres1 = document.getElementById("shres1");
+let count1 = document.getElementById("count1");
+let wrong1 = document.getElementById("wrong1");
+let score = 0;
+let wrong11 = "";
+let length1 = 0;
+count1.onclick = function(){
+    ans1 = ""+document.getElementById("ans1").value;
+    if(ans1=="8"){
+        score++;
+    }
+    else{
+        wrong11 += "1, ";
+    }
+    ans2 = ""+document.getElementById("ans2").value;
+    if(ans2=="22"){
+        score++;
+    }
+    else{
+        wrong11 += "2, ";
+    }
+    ans3 = ""+document.getElementById("ans3").value;
+    if(ans3=="7"){
+        score++;
+    }
+    else{
+        wrong11 += "3, ";
+    }
+    ans4 = ""+document.getElementById("ans4").value;
+    if(ans4=="73"){
+        score++;
+    }
+    else{
+        wrong11 += "4, ";
+    }
+    ans5 = ""+document.getElementById("ans5").value;
+    if(ans5=="14"){
+        score++;
+    }
+    else{
+        wrong11 += "5, ";
+    }
+    ans6 = ""+document.getElementById("ans6").value;
+    if(ans6=="18"){
+        score++;
+    }
+    else{
+        wrong11 += "6, ";
+    }
+    ans7 = ""+document.getElementById("ans7").value;
+    if(ans7=="14"){
+        score++;
+    }
+    else{
+        wrong11 += "7, ";
+    }
+    ans8 = ""+document.getElementById("ans8").value;
+    if(ans8=="15"){
+        score++;
+    }
+    else{
+        wrong11 += "8, ";
+    }
+    ans9 = ""+document.getElementById("ans9").value;
+    if(ans9=="853"){
+        score++;
+    }
+    else{
+        wrong11 += "9, ";
+    }
+    ans10 = ""+document.getElementById("ans10").value;
+    if(ans10=="351"){
+        score++;
+    }
+    else{
+        wrong11 += "10, ";
+        length1 += 1;
+    }
+    length1 += ((10-score)*3)-2;
+    wrong1.textContent = wrong11.slice(0,length1);
+    shres1.textContent = score;
+}
